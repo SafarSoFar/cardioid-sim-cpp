d7b83ba40f88dee56309c89f7f58d9a03da8e99d
SafarSoFar <so_far_away0525@proton.me>
2024-12-04 16:45:07 +0500

    no matmul, easier approach

--- main.cpp
+++ main.cpp
@@ -26,26 +26,16 @@ Vector2 operator/(Vector2 lhs, float rhs){
   return Vector2{lhs.x/rhs, lhs.y/rhs};
 }
 
-Vector2 matMul(Vector2 vec2D, std::vector<std::vector<float>> mat){
-  /*if(mat[0].size() != 3 || mat.size() != 3){*/
-  /*  return Vector3Zero();*/
-  /*}*/
-  float x = (vec2D.x*mat[0][0])+(vec2D.y*mat[0][1]);
-  float y = (vec2D.x*mat[1][0])+(vec2D.y*mat[1][1]);
-  Vector2 vec = Vector2{x,y};
-  return vec;
-};
 
-Vector2 zAxisRotation(Vector2 vec2, float angle){
-  float radians = angle*DEG2RAD;
-  float cosine = cos(radians);
-  float sine = sin(radians);
-  std::vector<std::vector<float>> rotZMat ={
-    {cosine,-sine,0},
-    {sine,cosine,0}
-  };
-  Vector2 rotatedVec = matMul(vec2, rotZMat);
-  return rotatedVec;
+Vector2 surfacePointRot(float radius,float angle){
+  float rad = angle * DEG2RAD;
+  return Vector2{radius*cos(rad),radius*sin(rad)};
+}
+
+struct Line{
+  Vector2 start;
+  Vector2 end;
+  Color color;
 };
 
 
@@ -56,24 +46,18 @@ int main(void)
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cardioid");
 
-    SetTargetFPS(60);
+    SetTargetFPS(300);
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     ClearBackground(BLACK);
-
-
-    float radius = 400;
-    Vector2 vals[divisions];
-    for(int i = 0; i < divisions; i++){
-      Vector2 vec = Vector2{radius,0};
-      vec = zAxisRotation(vec, (360.0/divisions)*i);
-      vals[i] = vec;
-    }
 
     Vector2 centerVec = Vector2{SCREEN_WIDTH/2.0, SCREEN_HEIGHT/2.0};
 
-    Vector2 curPos = centerVec+vals[0];
     int curIndex = 0;
-    double multiplier = 1.0;
-    std::pair<Vector2,Vector2>lines[divisions];
+    double multiplier = 4.0;
+    Line lines[divisions];
+
+    Color curColor = {0,0,0,255};
+    int hue = 0;
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -81,16 +65,33 @@ int main(void)
 
         DrawCircleLines(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 400, WHITE);
 
-        int connectionIndex = (int(curIndex*multiplier))%divisions;
-        Vector2 nextPos = centerVec+vals[connectionIndex];
-        lines[curIndex%divisions] = {centerVec+vals[curIndex%divisions], nextPos};
-
+        float radius = 400;
+        Vector2 vals[divisions];
         for(int i = 0; i < divisions; i++){
-          DrawLineV(lines[i].first,lines[i].second, WHITE);
+          Vector2 vec = surfacePointRot(radius, (360.0/divisions)*i);
+
+          vals[i] = vec;
         }
 
-        multiplier += 0.1;
+
+        Vector2 curPos = centerVec+vals[0];
+
+        int connectionIndex = (int(curIndex*multiplier))%divisions;
+        Vector2 nextPos = centerVec+vals[connectionIndex];
+
+        hue++;
+        if(hue > 360){
+          hue = 0;
+        }
+        curColor = ColorFromHSV(hue, 1.0, 1.0);
+
+        lines[curIndex%divisions] = {centerVec+vals[curIndex%divisions], nextPos, WHITE};
+
+        for(int i = 0; i < divisions; i++){
+          DrawLineEx(lines[i].start,lines[i].end, 1.0,lines[i].color);
+        }
         curIndex++;
+        multiplier += 0.01;
         ClearBackground(BLACK);
 
         
